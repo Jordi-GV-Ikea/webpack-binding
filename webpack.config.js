8081b51c94de96e20cb31029f15058dac478c1f3
Jordi-GV-Ikea <jordi.gonzalez.verges@ingka.com>
2022-06-01 10:30:58 +0200

    output management finish

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,19 @@
-const path = require('path');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
- module.exports = {
-   entry: './src/index.js',
-   output: {
-     filename: 'bundle.js',
-     path: path.resolve(__dirname, 'dist'),
-   },
- };
+module.exports = {
+  entry: {
+    index: "./src/index.js",
+    print: "./src/print.js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Output management",
+    }),
+  ],
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+};
