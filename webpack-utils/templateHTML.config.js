17864292ff9daf9aa2c5ad83d97381a087910189
Jordi-GV-Ikea <jordi.gonzalez.verges@ingka.com>
2022-06-02 09:24:39 +0200

    footer

--- webpack-utils/templateHTML.config.js
+++ webpack-utils/templateHTML.config.js
@@ -1,12 +1,12 @@
 const environment = "prod"; // cte || ppe || prod;
 
 module.exports = function HTMLconfig() {
-  const title = "Customer service – how can we help you?";
   const template = "./public/template.html";
-  const googleAnalyticsId = "'UA-101279823-34'";
+  const title = "Customer service – how can we help you?";
   const metaContent = JSON.stringify(
     "Want to contact IKEA? No problem. Contact us today via phone, email or chat. Select the topic and we will help you on the right channel."
   );
+  const googleAnalyticsId = "'UA-101279823-34'";
 
   if (environment === "cte") {
     return {
@@ -19,6 +19,7 @@ module.exports = function HTMLconfig() {
       headerSrc: JSON.stringify(
         "https://cte.ikeadt.com/au/en/header-footer/style-fragment-recursive.html?request-path=$(REQUEST_PATH)"
       ),
+      footerSrc: JSON.stringify("https://cte.ikeadt.co/au/en/header-footer/header-fragment-recursive.html"),
     };
   } else if (environment === "ppe") {
     return {
@@ -31,6 +32,7 @@ module.exports = function HTMLconfig() {
       headerSrc: JSON.stringify(
         "https://ppe.ikeadt.com/au/en/header-footer/style-fragment-recursive.html?request-path=$(REQUEST_PATH)"
       ),
+      footerSrc: JSON.stringify("https://ppe.ikeadt.co/au/en/header-footer/header-fragment-recursive.html"),
     };
   } else if (environment === "prod") {
     return {
@@ -43,6 +45,7 @@ module.exports = function HTMLconfig() {
       headerSrc: JSON.stringify(
         "https://www.ikea.com/au/en/header-footer/style-fragment-recursive.html?request-path=$(REQUEST_PATH)"
       ),
+      footerSrc: JSON.stringify("https://www.ikea.com/au/en/header-footer/header-fragment-recursive.html"),
     };
   }
 };
