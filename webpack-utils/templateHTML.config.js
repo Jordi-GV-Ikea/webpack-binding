984a61985f21de56bc873d19405c7d7012dbc5d2
Jordi-GV-Ikea <jordi.gonzalez.verges@ingka.com>
2022-06-02 08:50:32 +0200

    header

--- webpack-utils/templateHTML.config.js
+++ webpack-utils/templateHTML.config.js
@@ -1,28 +1,48 @@
-const environment = 'prod';
+const environment = "prod"; // cte || ppe || prod;
 
-module.exports =  function HTMLconfig  ()  {
-    const title = 'Customer service – how can we help you?';
-    const template= './public/template.html';
-    const googleAnalyticsId= "'UA-101279823-34'";
-    const metaContent = JSON.stringify("Want to contact IKEA? No problem. Contact us today via phone, email or chat. Select the topic and we will help you on the right channel.");
-        
-    if (environment === 'prod')
-      return {
-        title,
-        template,
-        googleAnalyticsId,
-        metaContent,
-        myHeader: 'My Header Tag',
-        canonicalLink: 'https://www.ikea.com/gb/en/customer-service/support',
-      };
-    else
-      return {
-        title,
-        template,
-        googleAnalyticsId,
-        metaContent,
-        myHeader: 'My Header Tag minor than 5',
-        canonicalLink: 'https://cte.ikeadt.com/gb/en/customer-service/support',
-      };
+module.exports = function HTMLconfig() {
+  const title = "Customer service – how can we help you?";
+  const template = "./public/template.html";
+  const googleAnalyticsId = "'UA-101279823-34'";
+  const metaContent = JSON.stringify(
+    "Want to contact IKEA? No problem. Contact us today via phone, email or chat. Select the topic and we will help you on the right channel."
+  );
+
+  if (environment === "cte") {
+    return {
+      title,
+      template,
+      googleAnalyticsId,
+      metaContent,
+      myHeader: "My Header Tag minor than 5",
+      canonicalLink: "https://cte.ikeadt.com/gb/en/customer-service/support",
+      headerSrc: JSON.stringify(
+        "https://cte.ikeadt.com/au/en/header-footer/style-fragment-recursive.html?request-path=$(REQUEST_PATH)"
+      ),
+    };
+  } else if (environment === "ppe") {
+    return {
+      title,
+      template,
+      googleAnalyticsId,
+      metaContent,
+      myHeader: "My Header Tag minor than 5",
+      canonicalLink: "https://ppe.ikeadt.com/gb/en/customer-service/support",
+      headerSrc: JSON.stringify(
+        "https://ppe.ikeadt.com/au/en/header-footer/style-fragment-recursive.html?request-path=$(REQUEST_PATH)"
+      ),
+    };
+  } else if (environment === "prod") {
+    return {
+      title,
+      template,
+      googleAnalyticsId,
+      metaContent,
+      myHeader: "My Header Tag",
+      canonicalLink: "https://www.ikea.com/gb/en/customer-service/support",
+      headerSrc: JSON.stringify(
+        "https://www.ikea.com/au/en/header-footer/style-fragment-recursive.html?request-path=$(REQUEST_PATH)"
+      ),
+    };
   }
-  
+};
