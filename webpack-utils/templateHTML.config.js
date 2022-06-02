1f9d1803c8a7367d11acccb697f823fbe7475f24
Jordi-GV-Ikea <jordi.gonzalez.verges@ingka.com>
2022-06-02 12:42:08 +0200

    completed all values

--- webpack-utils/templateHTML.config.js
+++ webpack-utils/templateHTML.config.js
@@ -1,51 +1,63 @@
+const cteHTML = {
+  template: "./public/template.html",
+  googleAnalyticsId: "'UA-101279823-34'",
+  title: "Customer service – how can we help you?",
+  metaContent: JSON.stringify(
+    "Want to contact IKEA? No problem. Contact us today via phone, email or chat. Select the topic and we will help you on the right channel."
+  ),
+  canonicalLink: "https://cte.ikeadt.com/gb/en/customer-service/support",
+  preconectLink: JSON.stringify(("https:/cte.ikeadt.com")),
+  headerFooterStyleSrc: JSON.stringify("https://cte.ikeadt.com/au/en/header-footer/style-fragment-recursive.html?request-path=$(REQUEST_PATH)"
+  ),
+  headerSrc: JSON.stringify(
+    "https://cte.ikeadt.co/au/en/header-footer/header-fragment-recursive.html"
+  ),
+  footerSrc: JSON.stringify("https://cte.ikeadt.com/au/en/header-footer/footer-fragment-recursive.html"),
+  headerFooterScriptSrc: JSON.stringify("https://cte.ikeadt.com/au/en/header-footer/script-fragment-recursive.html?request-path=$(REQUEST_PATH)")
+};
+
+const ppeHTML = {
+  template: "./public/template.html",
+  googleAnalyticsId: "'UA-101279823-34'",
+  title: "Customer service – how can we help you?",
+  metaContent: JSON.stringify(
+    "Want to contact IKEA? No problem. Contact us today via phone, email or chat. Select the topic and we will help you on the right channel."
+  ),
+  canonicalLink: "https://ppe.ikeadt.com/gb/en/customer-service/support",
+  preconectLink: JSON.stringify("https:/ppe.ikeadt.com"),
+  headerFooterStyleSrc: JSON.stringify(
+    "https://ppe.ikeadt.com/au/en/header-footer/style-fragment-recursive.html?request-path=$(REQUEST_PATH)"
+  ),
+  headerSrc: JSON.stringify(
+    "https://ppe.ikeadt.com/au/en/header-footer/header-fragment-recursive.html"
+  ),
+  footerSrc: JSON.stringify("https://ppe.ikeadt.com/au/en/header-footer/footer-fragment-recursive.html"),
+  headerFooterScriptSrc: JSON.stringify("https://ppe.ikeadt.com/au/en/header-footer/script-fragment-recursive.html?request-path=$(REQUEST_PATH)")
+};
+
+const prodHTML = {
+  template: "./public/template.html",
+  googleAnalyticsId: "'UA-101279823-34'",
+  title: "Customer service – how can we help you?",
+  metaContent: JSON.stringify(
+    "Want to contact IKEA? No problem. Contact us today via phone, email or chat. Select the topic and we will help you on the right channel."
+  ),
+  canonicalLink: "https://www.ikea.com/gb/en/customer-service/support",
+  preconectLink: JSON.stringify("https://www.ikea.com"),
+  headerFooterStyleSrc: JSON.stringify(
+    "https://www.ikea.com/au/en/header-footer/style-fragment-recursive.html?request-path=$(REQUEST_PATH)"
+  ),
+  headerSrc: JSON.stringify(
+    "https://www.ikea.com/au/en/header-footer/header-fragment-recursive.html"
+  ),
+  footerSrc: JSON.stringify("https://www.ikea.com/au/en/header-footer/footer-fragment-recursive.html"),
+  headerFooterScriptSrc: JSON.stringify("https://www.ikea.com/au/en/header-footer/script-fragment-recursive.html?request-path=$(REQUEST_PATH)")
+};
+
 const environment = "prod"; // cte || ppe || prod;
 
 module.exports = function HTMLconfig() {
-  const template = "./public/template.html";
-  const title = "Customer service – how can we help you?";
-  const metaContent = JSON.stringify(
-    "Want to contact IKEA? No problem. Contact us today via phone, email or chat. Select the topic and we will help you on the right channel."
-  );
-  const googleAnalyticsId = "'UA-101279823-34'";
-
-  if (environment === "cte") {
-    return {
-      title,
-      template,
-      googleAnalyticsId,
-      metaContent,
-      myHeader: "My Header Tag minor than 5",
-      canonicalLink: "https://cte.ikeadt.com/gb/en/customer-service/support",
-      headerSrc: JSON.stringify(
-        "https://cte.ikeadt.com/au/en/header-footer/style-fragment-recursive.html?request-path=$(REQUEST_PATH)"
-      ),
-      footerSrc: JSON.stringify("https://cte.ikeadt.co/au/en/header-footer/header-fragment-recursive.html"),
-    };
-  } else if (environment === "ppe") {
-    return {
-      title,
-      template,
-      googleAnalyticsId,
-      metaContent,
-      myHeader: "My Header Tag minor than 5",
-      canonicalLink: "https://ppe.ikeadt.com/gb/en/customer-service/support",
-      headerSrc: JSON.stringify(
-        "https://ppe.ikeadt.com/au/en/header-footer/style-fragment-recursive.html?request-path=$(REQUEST_PATH)"
-      ),
-      footerSrc: JSON.stringify("https://ppe.ikeadt.co/au/en/header-footer/header-fragment-recursive.html"),
-    };
-  } else if (environment === "prod") {
-    return {
-      title,
-      template,
-      googleAnalyticsId,
-      metaContent,
-      myHeader: "My Header Tag",
-      canonicalLink: "https://www.ikea.com/gb/en/customer-service/support",
-      headerSrc: JSON.stringify(
-        "https://www.ikea.com/au/en/header-footer/style-fragment-recursive.html?request-path=$(REQUEST_PATH)"
-      ),
-      footerSrc: JSON.stringify("https://www.ikea.com/au/en/header-footer/header-fragment-recursive.html"),
-    };
-  }
+  if (environment === "cte") return cteHTML;
+  else if (environment === "ppe") return ppeHTML;
+  else if (environment === "prod") return prodHTML;
 };
